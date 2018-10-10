247bee6429edbd725b2e21df6a7e156a2833dc67
brianboisvert <b.m.boisvert@gmail.com>
2018-10-10 14:07:25 -0400

    memoized fib

--- fib/index.js
+++ fib/index.js
@@ -8,21 +8,56 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-  let fib = []
-
-  for (let  i = 0; i <= n; i++) {
-    if (i === 0) {
-      fib.push(0);
-    } else if (i === 1) {
-      fib.push(1);
-    } else {
-      let num = fib[i - 2] + fib[i - 1];
-      fib.push(num)
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
     }
-  }
 
-  return fib[n];
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
 }
 
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
+
+
 module.exports = fib;
+
+// function fib(n) {
+//   let fib = []
+//
+//   for (let  i = 0; i <= n; i++) {
+//     if (i === 0) {
+//       fib.push(0);
+//     } else if (i === 1) {
+//       fib.push(1);
+//     } else {
+//       let num = fib[i - 2] + fib[i - 1];
+//       fib.push(num)
+//     }
+//   }
+//
+//   return fib[n]
+// }
+
+
+
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+//
+//   return fib(n - 1) + fib(n - 2);
+// }
